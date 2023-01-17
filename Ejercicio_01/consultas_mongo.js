de3e1d38a5064173750b7923a7b7a4acd0fc6b03
jpromanonet <programming@jpromano.net>
2023-01-16 21:20:56 -0300

    Ejercicio 01

---
+++ Ejercicio_01/consultas_mongo.js
@@ -0,0 +1,20 @@
+// Punto 01
+use empresa
+
+// Punto 2
+show collections
+
+// Punto 3
+db.clientes.insertMany([{nombre: "Gonzalo", edad: "47"}, {nombre: "Camila", edad: "30"}])
+
+// Punto 4
+db.createCollection("articulos")
+
+// Punto 5
+db.articulos.insert([{nombre: "Bizcochitos don satur salados", precio: "150", stock: "100"}, 
+                     {nombre: "Bizcochitos don satur dulces", precio: "150", stock : "100"}, 
+                     {nombre: "Crema", precio: "185", stock: "50"}, 
+                     {nombre: "Harina", precio: "223", stock: "1000"}])
+
+// Punto 6
+show collections
